6a58f91fbef5196e9054df8bbbdf9520680e1246
ketanghosh-dev <ketanghosh.dev@gmail.com>
2025-10-19 19:54:12 +0530

    Vowel Check using If else

---
+++ basic/control_statement/if_else/vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char t;
+    printf("Enter Alphabet:\n");
+    scanf("%c",&t);
+    if(t=='a' || t=='A' || t=='e' || t=='E'
+    || t=='i' || t=='I' || t=='o' || t=='O' ||t=='u' ||
+t=='U')
+{printf("%c is Vowel",t);}
+else
+printf("%c is consonant",t);
+
+
+
+    return 0;
+}
