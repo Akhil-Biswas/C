157cffb78f6900a0a372194abd25b5a74736bcb3
ketanghosh-dev <ketanghosh.dev@gmail.com>
2025-12-19 17:02:46 +0530

    my first commit

---
+++ basic/control_statement/loop/for/perfect_nos_hard.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+    printf("Enter Number: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        sum=0;                      // Reset step is imp
+        for(int j=1;j<i;j++){       // j always less than i
+            if(i%j==0){
+                sum=sum+j;
+            }
+        }
+        if(i==sum){             // printing i inside 1st loop
+            printf(" %d ",i);
+        }
+    }
+    return 0;
+}
