1fe4dbfade9a12d59b4024de82d264945e06d59b
gouri <biswas.gouri90@gmail.com>
2026-01-12 00:56:21 +0530

    Solution of Q14,15

---
+++ problem_solving/PCA/15.c
@@ -0,0 +1,31 @@
+// ########################################
+// WAP to input consumed unit and calculate Telephone Bill (Monthly rent ₹300).
+
+//      Consumed Unit |	Charge per unit (Rs.)
+//      First 300	  |           7
+//      Next 500	  |           9
+//      Next 700	  |           12
+//      1501 & above. |           15
+// ########################################
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    int units = 0, bill = 300;
+    clrscr();
+    printf("Enter consumed units: ");
+    scanf("%d",&units);
+    if(units <= 300){
+        bill = bill + (units * 7;)
+    }
+    else if(units <= 700){
+        bill = bill + (300 * 7) + ((units - 300) * 9);
+    }
+    else if(units <= 1500){
+        bill = bill + (300 * 7) + (500 * 9) + ((units - 700) * 12);
+    }
+    else{
+        bill = bill + (300 * 7) + (500 * 9) + (700 * 12) + ((units - 1500) * 15);
+    }
+    printf("Total Electricity Bill: Rs.%d", bill);
+    getch();
+}
